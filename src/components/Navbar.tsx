82083b2beb169a97570aae9ef83625d261bff270
tiru_venkatesh <tiruvenkatesh123@gmail.com>
2026-07-24 14:09:38 +0530

    Add logo image import to Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,6 +24,7 @@ import {
   Check
 } from "lucide-react";
 import { Notification } from "../types";
+import logo from "../assets/1.jpg"; // add this line
 import { CITIES_DATA } from "../data/cityData";
 
 interface NavbarProps {
